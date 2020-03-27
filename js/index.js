604699fa07a0515364b97a487b6c7bf3aa0f9b6a
wwolman <56605044+wwolman@users.noreply.github.com>
2020-03-27 14:13:17 -0400

    Check third commit

--- js/index.js
+++ js/index.js
@@ -12,6 +12,17 @@ let maxX = docW - winW
 let pctY = Math.round(winY / Math.max(maxY, 1) * 100)
 let pctX = Math.round(winX / Math.max(maxX, 1) * 100)
 
+let $third = document.querySelector('.full-viewport:nth-child(3)')
+let thirdTop = $third.getBoundingClientRect().top
+
+
+if (thirdTop < winH / 2){
+    console.log(`Above Viewport Half`)
+} else {
+    console.log(`Below viewport half`)
+}
+
+
 document.querySelector('.panel').innerHTML = `
 <li>The Window is <strong>${winH}</strong> px tall, <strong>${winW}</strong> px wide </li>
 <li>The Document is <strong>${docH}</strong> px tall, <strong>${docW}</strong> px wide </li>
@@ -20,6 +31,8 @@ document.querySelector('.panel').innerHTML = `
         <li>That's <strong>${pctY}</strong> % of <strong>${maxY}</strong> vertically </li>
         <li>That's <strong>${pctX}</strong>  % of <strong>${maxX}</strong> horizontally </li>
     </ol>
+    <li>The third section is <strong>${$third.offsetTop}</strong> px from the top of the document</li>
+    <li>The third section is <strong>${$third.getBoundingClientRect().top}</strong> from the top of the viewport / window</li>
 `
 }
 
